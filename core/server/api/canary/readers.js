5a68a9be5bc1e2e41e2ee5632125a8fb5535ad0a
Khue Vu <khuevu@gmail.com>
2019-09-17 22:35:03 +0100

    minor

--- core/server/api/canary/readers.js
+++ core/server/api/canary/readers.js
@@ -2,6 +2,7 @@ const models = require('../../models');
 const common = require('../../lib/common');
 const urlUtils = require('../../lib/url-utils');
 const UNSAFE_ATTRS = ['status', 'roles'];
+const debug = require('ghost-ignition').debug('api:canary:utils:serializers:input:users');
 
 module.exports = {
     docName: 'readers',
@@ -16,6 +17,7 @@ module.exports = {
             unsafeAttrs: UNSAFE_ATTRS
         },
         query(frame) {
+            //TODO: update roles as reader 
             return models.User.add(frame.data.readers[0], frame.options)
                 .then((model) => {
                     return model;
